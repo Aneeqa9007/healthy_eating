65b820980aea9085d59f8c74368defc0363df0ed
Aneeqa9007 <138521630+Aneeqa9007@users.noreply.github.com>
2024-06-08 14:24:41 +0530

    Add files via upload

---
+++ recipe.js
@@ -0,0 +1,11 @@
+// window.onload = function() {
+//     document.body.classList.add('loaded');
+//   };
+  
+
+const scroll = new LocomotiveScroll({
+    el: document.querySelector('[data-scroll-container]'),
+    smooth: true,
+    lerp:0.03
+});
+
